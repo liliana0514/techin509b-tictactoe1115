938fb935dc5f9093f73941b94003d31c2f12a4a4
liliana0514 <148395601+liliana0514@users.noreply.github.com>
2023-11-12 18:37:06 -0800

    Update cli.py

--- cli.py
+++ cli.py
@@ -5,28 +5,28 @@
 
 from logic import Player, Game
 
+import random
+
+
 if __name__ == "__main__":
     player1_name = input("Enter name for Player 1: ")
     player1_symbol = input("Enter symbol for Player 1 (X or O): ")
     player1 = Player(player1_name, player1_symbol)
 
     if player1_symbol.upper() == 'X':
-        player2_symbol = 'O'
+        player2 = Player("Bot", 'O')
     elif player1_symbol.upper() == 'O':
-        player2_symbol = 'X'
+        player2 = Player("Bot", 'X')
     else:
         print("Invalid symbol for Player 1. Exiting.")
         exit()
 
-    player2 = Player("Bot", player2_symbol)
-
     game_mode = input("Enter game mode (1 for single player, 2 for two players): ")
 
-    if game_mode == '1':
-        player2 = Player("Bot", player2_symbol)
-    elif game_mode != '2':
+    if game_mode != '1' and game_mode != '2':
         print("Invalid game mode. Exiting.")
         exit()
 
     game = Game(player1, player2)
     game.start_game()
+
